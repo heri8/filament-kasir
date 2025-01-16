2f1963c2b678437a7b9c38afbcb109a2cd6cd299
heri <hsetiawan8.hs@gmail.com>
2025-01-16 12:00:32 +0700

    add crud product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,13 +5,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'category_id',
+        'slug',
+        'stock',
+        'price',
+        'is_active',
+        'image',
+        'barcode',
+        'description'
+    ];
+
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Category::class);
+    }
+
+    public static function generateUniqueSlug(string $name): string
+    {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while (self::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
     }
 }
